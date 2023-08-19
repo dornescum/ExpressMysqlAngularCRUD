7f6fb8fd17ff38453d62332d582147db2f23c357
Mihai Dornescu <dornescu@gmail.com>
2023-08-19 10:06:07 +0300

    register add items

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -7,11 +7,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {PasswordModule} from 'primeng/password';
 import {ButtonModule} from 'primeng/button';
 import {CheckboxModule} from 'primeng/checkbox';
+import {InputNumberModule} from "primeng/inputnumber";
+import {InputTextModule} from "primeng/inputtext";
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-	imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, PasswordModule, ButtonModule, CheckboxModule]
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, PasswordModule, ButtonModule, CheckboxModule, InputNumberModule, InputTextModule]
 })
 export class AuthModule {
 }
--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -15,6 +15,9 @@ export class RegisterComponent implements OnInit{
   confirmPassword = '';
   error = '';
   loading = false;
+  age!: number;
+  nickname!: string;
+  agree!: boolean;
 
   constructor(private router: Router, private auth: AuthService) {}
 
@@ -24,22 +27,27 @@ export class RegisterComponent implements OnInit{
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
       confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      agree: new FormControl<string | null>(null)
-
+      agree: new FormControl<string | null>(null),
+      age: new FormControl<number | null>(null),
+      nickname: new FormControl<string | null>(null),
     });
-
   }
 
-  navigateToRegister() {
-    this.router.navigate(['/auth/register']);
+  navigateToLogin() {
+    this.router.navigate(['/auth/login']);
   }
 
   onSubmit(): void {
+    console.log('form', this.formGroup.value )
+
     this.loading = true;
     this.error = '';
     this.email = this.formGroup.get('email')?.value.toLowerCase();
     this.password = this.formGroup.get('password')?.value.toLowerCase();
     this.confirmPassword = this.formGroup.get('confirmPassword')?.value.toLowerCase();
+    this.age = this.formGroup.get('age')?.value.toLowerCase();
+    this.nickname = this.formGroup.get('nickname')?.value.toLowerCase();
+    this.agree = this.formGroup.get('agree')?.value.toLowerCase();
     if (!this.email || !this.password) {
       this.error = 'Make sure to fill everything ;)';
     }
@@ -47,22 +55,22 @@ export class RegisterComponent implements OnInit{
       this.error = 'Passwords need to match';
     }
 
-    else {
-      this.auth
-        .register({ email: this.email, password: this.password })
-        .subscribe(
-          (res) => {
-            console.log('response login', res)
-            this.loading = false;
-            this.router.navigate(['/auth/login']);
-          },
-          (err) => {
-            console.log(err);
-            this.error = err.error.message;
-            this.loading = false;
-          }
-        );
-    }
+    // else {
+    //   this.auth
+    //     .register({ email: this.email, password: this.password })
+    //     .subscribe(
+    //       (res) => {
+    //         console.log('response login', res)
+    //         this.loading = false;
+    //         this.router.navigate(['/auth/login']);
+    //       },
+    //       (err) => {
+    //         console.log(err);
+    //         this.error = err.error.message;
+    //         this.loading = false;
+    //       }
+    //     );
+    // }
   }
 
 }
